f57fff207ae69ba736b95c656591b99031c7f35f
gitCauchy <mailcauchy@gmail.com>
2019-12-11 17:25:51 +0800

    二叉树内部类实现

--- src/com/cauchy/tree/binarytree/BinaryTreeByInnerClass.java
+++ src/com/cauchy/tree/binarytree/BinaryTreeByInnerClass.java
@@ -10,8 +10,8 @@ package com.cauchy.tree.binarytree;
  *
  */
 public class BinaryTreeByInnerClass {
-	
-	public class TreeNode{
+
+	public class TreeNode {
 		/*
 		 * 左子节点
 		 */
@@ -24,152 +24,160 @@ public class BinaryTreeByInnerClass {
 		 * 值
 		 */
 		int value;
-		/**
-		 * @description 设置左节点
-		 * @param lNode 
-		 */
-		public void setlNode(TreeNode lNode) {
-			this.lNode = lNode;
-		}
-		/**
-		 * @description 设置右节点
-		 * @param rNode
-		 */
-		public void setrNode(TreeNode rNode) {
-			this.rNode = rNode;
-		}
-		/**
-		 * @description 构造一个节点
-		 * @param value
-		 */
-		public TreeNode(int value) {
-			this.value = value;
-		}
-		
-		public String toString() {
-			return "TreeNode [value=" + value + "]";
-		}
 
+		/**
+		 * @Description 先序遍历
+		 */
 		public void preOrderTraversal() {
-			System.out.print(this.value + "--");
-			if(lNode != null) {
+			System.out.print(this.value + "-");
+			if (lNode != null) {
 				lNode.preOrderTraversal();
 			}
-			if(rNode != null) {
+			if (rNode != null) {
 				rNode.preOrderTraversal();
 			}
 		}
-		
+
+		/**
+		 * @Description 中序遍历
+		 */
 		public void inOrderTraversal() {
-			if(lNode != null) {
+			if (lNode != null) {
 				lNode.inOrderTraversal();
 			}
-			System.out.print(this.value + "--");
-			if(rNode != null) {
+			System.out.print(this.value + "-");
+			if (rNode != null) {
 				rNode.inOrderTraversal();
 			}
 		}
-		
+
+		/**
+		 * @Description 后序遍历
+		 */
 		public void postOrderTraversal() {
-			
-			if(lNode != null) {
+			if (lNode != null) {
 				lNode.postOrderTraversal();
 			}
-			if(rNode != null) {
+			if (rNode != null) {
 				rNode.postOrderTraversal();
 			}
-			System.out.print(this.value + "--");
+			System.out.print(this.value + "-");
 		}
-		
-		public TreeNode preOrderSearch(int x){
+
+		/**
+		 * 
+		 * @param x
+		 * @return
+		 * @Description 先序查找
+		 */
+		public TreeNode preOrderSearch(int x) {
 			TreeNode target = null;
-			if(this.value == x) {
+			if (this.value == x) {
 				return this;
-			}else {
-				if(lNode != null) {
+			} else {
+				if (lNode != null) {
 					target = lNode.preOrderSearch(x);
 				}
-				if(target != null) {
+				if (target != null) {
 					return target;
 				}
-				if(rNode != null) {
+				if (rNode != null) {
 					target = rNode.preOrderSearch(x);
 				}
-				if(target != null) {
+				if (target != null) {
 					return target;
 				}
 			}
 			return target;
 		}
-		
-		public TreeNode inOrderSearch(int x){
+
+		/**
+		 * 
+		 * @param x
+		 * @return
+		 * @Description 中序查找
+		 */
+		public TreeNode inOrderSearch(int x) {
 			TreeNode target = null;
-			if(lNode != null) {
+			if (lNode != null) {
 				target = lNode.inOrderSearch(x);
 			}
-			if(target != null) {
+			if (target != null) {
 				return target;
 			}
-			if(this.value == x) {
+			if (this.value == x) {
 				target = this;
 				return target;
-			}else {
-				if(rNode != null) {
+			} else {
+				if (rNode != null) {
 					target = rNode.inOrderSearch(x);
 				}
 			}
 			return target;
 		}
-		
-		public TreeNode postOrderSearch(int x){
+
+		/**
+		 * 
+		 * @param x
+		 * @return
+		 * @Description 后序查找
+		 */
+		public TreeNode postOrderSearch(int x) {
 			TreeNode target = null;
-			if(lNode != null) {
+			if (lNode != null) {
 				target = lNode.postOrderSearch(x);
 			}
-			if(target == null) {
-				if(rNode != null) {
+			if (target == null) {
+				if (rNode != null) {
 					target = rNode.postOrderSearch(x);
 				}
-				if(target == null) {
-					if(this.value == x) {
+				if (target == null) {
+					if (this.value == x) {
 						return this;
 					}
 				}
 			}
 			return target;
 		}
+
 		// 删除一棵子树
 		public void delete(int x) {
 			TreeNode parent = this;
-			if(parent.lNode != null && parent.lNode.value == x) {
+			if (parent.lNode != null && parent.lNode.value == x) {
 				parent.lNode = null;
 				return;
-			}else if(parent.rNode != null && parent.rNode.value == x) {
+			} else if (parent.rNode != null && parent.rNode.value == x) {
 				parent.rNode = null;
 				return;
-			}else {
+			} else {
 				parent = lNode;
-				if(parent != null) {
+				if (parent != null) {
 					parent.delete(x);
 				}
 				parent = rNode;
-				if(parent != null) {
+				if (parent != null) {
 					parent.delete(x);
 				}
 			}
 		}
+
+		public TreeNode(int value) {
+			this.value = value;
+		}
+
+		public String toString() {
+			return "TreeNode [value=" + value + "]";
+		}
 	}
 
 	BinaryTreeByInnerClass.TreeNode root;
-	
+
 	/**
 	 * @description 创建一颗空树
 	 */
-	public BinaryTreeByInnerClass() { 
+	public BinaryTreeByInnerClass() {
 		root = null;
 	}
-
-
 
 	/**
 	 * 
@@ -179,92 +187,81 @@ public class BinaryTreeByInnerClass {
 	 * @return 新建立的节点
 	 * @throws Exception
 	 */
-	public TreeNode setTreeNode(int data,TreeNode fNode,String nodeType) throws Exception {
-		
-		BinaryTreeByInnerClass.TreeNode node = this.new TreeNode(data);
-		if(fNode != null) {
-			if(nodeType.equals("left")) {
-				fNode.setlNode(node);
-			}else if(nodeType.equals("right")){
-				fNode.setrNode(node);
-			}else {
+	public TreeNode setTreeNode(int data, TreeNode fNode, String nodeType) throws Exception {
+
+		TreeNode node = new TreeNode(data);
+		if (fNode != null) {
+			if (nodeType.equals("left")) {
+				fNode.lNode = node;
+			} else if (nodeType.equals("right")) {
+				fNode.rNode = node;
+			} else {
 				throw new Exception("No such Type Node");
 			}
 		}
-		
+
 		return node;
 	}
+
 	/**
 	 * @description 树的先序遍历
 	 */
 	public void preOrderTraversal() {
+		System.out.println("--PRE ORDER TRAVERSAL--");
 		root.preOrderTraversal();
+		System.out.println();
 	}
+
 	/**
 	 * @description 树的中序遍历
 	 */
 	public void inOrderTraversal() {
+		System.out.println("--IN ORDER TRAVERSAL--");
 		root.inOrderTraversal();
+		System.out.println();
 	}
+
 	/**
 	 * @description 树的后序遍历
 	 */
 	public void postOrderTraversal() {
+		System.out.println("--POST ORDER TRAVERSAL--");
 		root.postOrderTraversal();
+		System.out.println();
 	}
-	
+	/**
+	 * 
+	 * @param x
+	 * @return
+	 * @Description 先序查找
+	 */
 	public TreeNode preOrderSearch(int x) {
 		return root.preOrderSearch(x);
 	}
-	
+	/**
+	 * 
+	 * @param x
+	 * @return
+	 * @Description 中序查找
+	 */
 	public TreeNode inOrderSearch(int x) {
 		return root.inOrderSearch(x);
 	}
-	
+	/**
+	 * 
+	 * @param x
+	 * @return
+	 * @Description 后序查找
+	 */
 	public TreeNode postOrderSearch(int x) {
 		return root.postOrderSearch(x);
 	}
-	
+	/**
+	 * 
+	 * @param x
+	 * @Description 删除节点
+	 */
 	public void deleteNode(int x) {
-		if(root.value == x) {
-			//root.delete(x);
-			root = null;
-		}else {
-			root.delete(x);
-		}
+		root.delete(x);
 	}
-	
-	
-	public static void main(String[] args) throws Exception {
-		BinaryTreeByInnerClass tree = new BinaryTreeByInnerClass();
-		//BinaryTree tree = new BinaryTree(this.setTreeNode(0, null, null));
-		tree.root = tree.setTreeNode(1, null, null);
-		BinaryTreeByInnerClass.TreeNode n21 = tree.setTreeNode(2, tree.root, "left");
-		BinaryTreeByInnerClass.TreeNode n22 = tree.setTreeNode(3, tree.root, "right");
-		BinaryTreeByInnerClass.TreeNode n31 = tree.setTreeNode(4, n21, "left");
-		BinaryTreeByInnerClass.TreeNode n32 = tree.setTreeNode(5, n21, "right");
-		BinaryTreeByInnerClass.TreeNode n33 = tree.setTreeNode(6, n22, "left");
-		BinaryTreeByInnerClass.TreeNode n34 = tree.setTreeNode(7, n22, "right");
-		
-		tree.deleteNode(2);
-		// 先序遍历
-		tree.preOrderTraversal();
-//		System.out.println();
-//		// 中序遍历
-//		tree.inOrderTraversal();
-//		System.out.println();
-//		// 后序遍历
-//		tree.postOrderTraversal();
-		System.out.println("---------------------");
-//		TreeNode result = tree.preOrderSearch(2);
-//		TreeNode result1 = tree.inOrderSearch(2);
-//		TreeNode result2 = tree.postOrderSearch(2);
-//		System.out.println(result);
-//		System.out.println(result1);
-//		System.out.println(result2);
-	}
-	
-	
-	
-	
 }
