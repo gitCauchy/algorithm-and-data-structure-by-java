834af7c7ab4281b77cf1270dd855d2bcfd4d79a9
gitCauchy <mailcauchy@gmail.com>
2019-12-12 19:18:26 +0800

    线索二叉树

--- src/com/cauchy/tree/linkedbinarytree/ThreadedBinaryTree.java
+++ src/com/cauchy/tree/linkedbinarytree/ThreadedBinaryTree.java
@@ -1,140 +1,159 @@
 package com.cauchy.tree.linkedbinarytree;
 
+/**
+ * 
+ * @author Cauchy
+ * @ClassName ThreadedBinaryTree.java
+ * @Date 2019年12月12日
+ * @Description 线索二叉树
+ * @Version V0.1
+ *
+ */
 public class ThreadedBinaryTree {
-	
-	
-	
 	/*
 	 * 根节点
 	 */
 	ThreadedNode root;
-	
+
+	/*
+	 * 临时存储前驱节点
+	 */
+	ThreadedNode preNode = null;
+
 	public ThreadedBinaryTree() {
 		root = null;
 	}
+
 	/**
 	 * 
 	 * @param data
 	 * @param fNode
 	 * @param nodeType
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 * @Description 插入节点
 	 */
-	public ThreadedNode buildTreeNode(int data,ThreadedNode fNode,String nodeType) throws Exception{
+	public ThreadedNode buildTreeNode(int data, ThreadedNode fNode, String nodeType) throws Exception {
 		ThreadedNode node = new ThreadedNode(data);
-		if(fNode != null) {
-			if(nodeType.equals("left")) {
+		if (fNode != null) {
+			if (nodeType.equals("left")) {
 				fNode.lNode = node;
-			}else if(nodeType.equals("right")) {
+			} else if (nodeType.equals("right")) {
 				fNode.rNode = node;
-			}else {
+			} else {
 				throw new Exception("No such type node");
 			}
 		}
 		return node;
 	}
+
 	/**
-	 * @Description 先序遍历二叉树
+	 * @Description 先序线索二叉树
 	 */
-	public void preOrderTraversal() {
-		root.preOrderTraversal();
+	public void threadPreOrderBinaryTree() {
+		threadPreOrderBinaryTree(root);
 	}
+
 	/**
-	 * @Description 中序遍历二叉树
+	 * @Description 中序线索二叉树
 	 */
-	public void inOrderTraversal() {
-		root.inOrderTraversal();
+	public void threadInOrderBinaryTree() {
+		threadInOrderBinaryTree(root);
 	}
+
 	/**
-	 * @Description 后序遍历二叉树
+	 * @Description 中序线索遍历
 	 */
-	public void postOrderTraversal() {
-		root.postOrderTraversal();
+	public void threadInOrderIterator() {
+		// 用于存储当前遍历的节点
+		ThreadedNode node = root;
+		while (node != null) {
+			// 循环找到最开始的节点
+			while (node.lType == 0) {
+				node = node.lNode;
+			}
+			// 打印当前节点的值
+			System.out.println(node.data);
+			// 如果当前节点的右指针指向的是后继节点，可能后继节点还有后继节点
+			while (node.rType == 1) {
+				node = node.rNode;
+				System.out.println(node.data);
+			}
+			// 替换遍历的节点
+			node = node.rNode;
+		}
 	}
+
 	/**
-	 * 
 	 * @param x
-	 * @return
-	 * @Description 先序查找
+	 * @Description 删除节点
 	 */
-	public ThreadedNode preOrderSearch(int x) {
-		return root.preOrderSearch(x);
-	}
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 * @Description 中序查找
-	 */
-	public ThreadedNode inOrderSearch(int x) {
-		return root.inOrderSearch(x);
-	}
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 * @Description 后序查找
-	 */
-	public ThreadedNode postOrderSearch(int x) {
-		return root.postOrderSearch(x);
-	}
-	
 	public void deleteNode(int x) {
-		if(root.data == x) {
-			root = null;
-		}else {
-			root.delete(x);
+
+	}
+
+	private void threadPreOrderBinaryTree(ThreadedNode node) {
+		// 处理当前节点
+		if (node.lNode == null) {
+			node.lNode = preNode;
+			node.lType = 1;
+		}
+		if (preNode != null && preNode.rNode == null) {
+			preNode.rNode = node;
+			preNode.rType = 1;
+		}
+		preNode = node;
+		// 处理左节点
+		if (node.lNode != null && node.lType == 0) {
+			threadPreOrderBinaryTree(node.lNode);
+		}
+		// 处理右节点
+		if (node.rNode != null && node.rType == 0) {
+			threadPreOrderBinaryTree(node.rNode);
 		}
 	}
-	// 临时存储前驱节点
-	ThreadedNode preNode = null;
-	
-	// 中序线索化二叉树
-	public void threadBinaryTree(ThreadedNode node) {
-		//当前节 点 如果为空，不处理
-		if(node == null) {
-			return;
-		}
+
+	/**
+	 * @param node
+	 * @Description 中序线索二叉树
+	 */
+	private void threadInOrderBinaryTree(ThreadedNode node) {
 		// 处理左子树
-		threadBinaryTree(node.lNode);
+		if (node.lNode != null) {
+			threadInOrderBinaryTree(node.lNode);
+		}
 		// 处理前驱节点
-		if(node.lNode == null) {
+		if (node.lNode == null) {
 			// 当前节点的左指针指向前驱节点，改变左指针类型
 			node.lNode = preNode;
 			node.lType = 1;
 		}
 		// 处理前驱节点的右指针
-		if(preNode != null && preNode.rNode == null) {
+		if (preNode != null && preNode.rNode == null) {
+			// 如果前驱节点的右指针为空，指向此节点
 			preNode.rNode = node;
 			preNode.rType = 1;
 		}
+		// 每处理一个节点，这个节点就是下一个节点的的前驱节点
 		preNode = node;
 		// 处理右子树
-		threadBinaryTree(node.rNode);
-	}
-	
-	public void threadIterator() {
-		// 用于存储当前遍历的节点
-		ThreadedNode node = root;
-		if(node != null) {
-			// 循环找到最开始的节点
-			while(node.lType == 0) {
-				node = node.lNode;
-			}
-			// 打印当前节点的值
-			System.out.println(node.data);
-			node = node.rNode;
-			// 如果当前节点的右指针指向的是后继节点，可能后继节点还有后继节点
-			while(node.rType == 1) {
-				node = node.rNode;
-				System.out.println(node.data);
-			}
-			node = node.rNode;
+		if (node.rNode != null) {
+			threadInOrderBinaryTree(node.rNode);
 		}
 	}
-	
-	public void threadBinaryTree() {
-		threadBinaryTree(root);
+
+	public static void main(String[] args) throws Exception {
+		ThreadedBinaryTree tree = new ThreadedBinaryTree();
+		tree.root = tree.buildTreeNode(1, null, null);
+		ThreadedNode n21 = tree.buildTreeNode(2, tree.root, "left");
+		ThreadedNode n22 = tree.buildTreeNode(3, tree.root, "right");
+		ThreadedNode n31 = tree.buildTreeNode(4, n21, "left");
+		ThreadedNode n32 = tree.buildTreeNode(5, n21, "right");
+		ThreadedNode n33 = tree.buildTreeNode(6, n22, "left");
+		ThreadedNode n34 = tree.buildTreeNode(7, n22, "right");
+		// tree.preOrderTraversal();
+		// tree.threadInOrderBinaryTree();
+		tree.threadPreOrderBinaryTree();
+		// tree.threadPreOrderIterator(tree.root);
 	}
 }
--- src/com/cauchy/tree/linkedbinarytree/ThreadedNode.java
+++ src/com/cauchy/tree/linkedbinarytree/ThreadedNode.java
@@ -1,5 +1,14 @@
 package com.cauchy.tree.linkedbinarytree;
 
+/**
+ * 
+ * @author Cauchy
+ * @ClassName ThreadedNode.java
+ * @Date 2019年12月12日
+ * @Description 线索二叉树节点
+ * @Version V0.1
+ *
+ */
 public class ThreadedNode {
 	/*
 	 * 数据
@@ -21,178 +30,13 @@ public class ThreadedNode {
 	 * 右指针类型
 	 */
 	int rType;
-	
-	/**
-	 * @Description 先序遍历
-	 */
-	public void preOrderTraversal() {
-		System.out.print(this.data + "-");
-		if(lNode != null) {
-			lNode.preOrderTraversal();
-		}
-		if(rNode != null) {
-			rNode.preOrderTraversal();
-		}
-	}
-	/**
-	 * @Description 中序遍历
-	 */
-	public void inOrderTraversal() {
-		if(lNode != null) {
-			lNode.inOrderTraversal();
-		}
-		System.out.print(this.data + "-");
-		if(rNode != null) {
-			rNode.inOrderTraversal();
-		}
-	}
-	/**
-	 * @Description 后序遍历
-	 */
-	public void postOrderTraversal() {
-		
-		if(lNode != null) {
-			lNode.postOrderTraversal();
-		}
-		if(rNode != null) {
-			rNode.postOrderTraversal();
-		}
-		System.out.print(this.data + "-");
-	}
-	/**
-	 * 
-	 * @param x
-	 * @return Node
-	 * @Description 先序查找
-	 */
-	public ThreadedNode preOrderSearch(int x){
-		ThreadedNode target = null;
-		if(this.data == x) {
-			return this;
-		}else {
-			if(lNode != null) {
-				target = lNode.preOrderSearch(x);
-			}
-			if(target != null) {
-				return target;
-			}
-			if(rNode != null) {
-				target = rNode.preOrderSearch(x);
-			}
-			if(target != null) {
-				return target;
-			}
-		}
-		return target;
-	}
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 * @Description 中序查找
-	 */
-	public ThreadedNode inOrderSearch(int x){
-		ThreadedNode target = null;
-		if(lNode != null) {
-			target = lNode.inOrderSearch(x);
-		}
-		if(target != null) {
-			return target;
-		}
-		if(this.data == x) {
-			target = this;
-			return target;
-		}else {
-			if(rNode != null) {
-				target = rNode.inOrderSearch(x);
-			}
-		}
-		return target;
-	}
-	/**
-	 * 
-	 * @param x
-	 * @return
-	 * @Description 后序查找
-	 */
-	public ThreadedNode postOrderSearch(int x){
-		ThreadedNode target = null;
-		if(lNode != null) {
-			target = lNode.postOrderSearch(x);
-		}
-		if(target == null) {
-			if(rNode != null) {
-				target = rNode.postOrderSearch(x);
-			}
-			if(target == null) {
-				if(this.data == x) {
-					return this;
-				}
-			}
-		}
-		return target;
-	}
-	
+
 	public ThreadedNode(int data) {
 		this.data = data;
-	}
-	
-	public ThreadedNode getlNode() {
-		return lNode;
-	}
-	
-	public void setlNode(ThreadedNode lNode) {
-		this.lNode = lNode;
-	}
-	
-	public ThreadedNode getrNode() {
-		return rNode;
-	}
-	
-	public void setrNode(ThreadedNode rNode) {
-		this.rNode = rNode;
-	}
-	
-	public int getValue() {
-		return data;
-	}
-	
-	public void setValue(int data) {
-		this.data = data;
-	}
-	
-	public void setValue() {
-		
 	}
 
 	public String toString() {
 		return "TreeNode [ value=" + data + "]";
 	}
-	/**
-	 * 
-	 * @param x
-	 * @Description 删除节点
-	 */
-	public void delete(int x) {
-		ThreadedNode parent = this;
-		if(parent.lNode != null && parent.lNode.data == x) {
-			parent.lNode = null;
-			return;
-		}else if(parent.rNode != null && parent.rNode.data == x) {
-			parent.rNode = null;
-			return;
-		}else {
-			parent = lNode;
-			if(parent != null) {
-				parent.delete(x);
-			}
-			parent = rNode;
-			if(parent != null) {
-				parent.delete(x);
-			}
-		}
-	}
-	
-	
-	
+
 }
